06961f057f16bf04b095bf73e471256b239eb7db
CloudChen <cloudchen2015@gmail.com>
2021-02-19 10:54:32 +0800

    1. Add BeforeStart, BeforeStop in SourceWorker and Worker 2. Add Defer in SourceWorker 3. Remove BeforeProcess in Worker

--- flow/worker.go
+++ flow/worker.go
@@ -7,12 +7,13 @@ import (
 
 // Worker Going worker interface
 type Worker interface {
+	BeforeStart() error
+	BeforeStop() error
+	Defer()
 	GetInfo() going.PluginInfo
 	GetLogger() logger.Logger
 	Init(wc WorkerCore, params string) error
-	Defer()
-	BeforeProcess() error
-	Process(data interface{}) (interface{}, error)
 	Notification(data interface{}) error
+	Process(data interface{}) (interface{}, error)
 	Timeout(seconds int, data interface{}) error
 }
