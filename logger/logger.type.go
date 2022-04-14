6ebd51446985c3947c515b1d01be2c51dcc805ca
ptoyoohri <leoho@bigobject.io>
2022-04-14 12:11:02 +0800

    add LogConfigure to flow to add rotate log.

--- logger/logger.type.go
+++ logger/logger.type.go
@@ -2,3 +2,9 @@ package logger
 
 // Defer logger defer call
 type Defer func()
+
+type LogConfigure struct {
+	Level      string  `json:"level"`
+	RotateHour float64 `json:"rotateHour"`
+	MaxHour    float64 `json:"maxHour"`
+}
